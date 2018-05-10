96554614ac070755a5bd0be845f92563f000e87a
T-R-K <t-r-k@libero.it>
2018-05-10 12:48:16 +0200

    rss

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+
+import { IonicStorageModule } from '@ionic/storage';
+import { FeedProvider } from './../providers/feed/feed';
+import { HttpModule } from '@angular/http';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
+
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FeedProvider,
+    InAppBrowser,
+    FeedProvider
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,66 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavController, AlertController, Nav, IonicPage } from 'ionic-angular';
+import { FeedProvider, Feed } from '../../providers/feed/feed';
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage = 'FeedListPage';
+  feeds: Feed[];
+
+  constructor(private navController: NavController, private feedProvider: FeedProvider, public alertCtrl: AlertController) { }
+
+  public addFeed() {
+    let prompt = this.alertCtrl.create({
+      title: 'Add Feed URL',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'The best Feed ever'
+        },
+        {
+          name: 'url',
+          placeholder: 'http://www.myfeedurl.com/feed'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            let newFeed = new Feed(data.name, data.url);
+            this.feedProvider.addFeed(newFeed).then(
+              res => {
+                this.loadFeeds();
+              }
+            );
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  private loadFeeds() {
+    this.feedProvider.getSavedFeeds().then(
+      allFeeds => {
+        this.feeds = allFeeds;
+      });
+  }
+
+  public openFeed(feed: Feed) {
+    this.nav.setRoot('FeedListPage', { 'selectedFeed': feed });
+  }
+
+  public ionViewWillEnter() {
+    this.loadFeeds();
+  }
+}
